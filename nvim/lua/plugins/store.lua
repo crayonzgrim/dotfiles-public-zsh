e21de5af4cf2bf00386bbce194ba2c80d32c3ecc
crayonzgrim <crayonzgrim@gmail.com>
2024-05-25 18:58:44 +0900

    🔨 Inlay hint text color change

--- nvim/lua/plugins/store.lua
+++ nvim/lua/plugins/store.lua
@@ -47,7 +47,7 @@ return {
 
       undotree.setup({
         float_diff = true, -- using float window previews diff, set this `true` will disable layout option
-        layout = "left_bottom", -- "left_bottom", "left_left_bottom"
+        layout = "left_left_bottom", -- "left_bottom", "left_left_bottom"
         ignore_filetype = { "undotree", "undotreeDiff", "qf", "TelescopePrompt", "spectre_panel", "tsplayground" },
         window = {
           winblend = 5,
